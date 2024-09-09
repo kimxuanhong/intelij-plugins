cd507da338e8e420c440acae3cdadefe218e2eba
hong.kim <hong.kim@bstarsolutions.com>
2024-09-09 11:47:55 +0700

    add logic remove vn accent

---
+++ src/main/kotlin/com/xhk/convertplugins/RemoveUnicodeAction.kt
@@ -0,0 +1,45 @@
+package com.xhk.convertplugins
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.SelectionModel
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+import java.util.*
+
+class RemoveUnicodeAction : AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        // Lấy đoạn văn bản được chọn
+        val editor = e.getRequiredData(CommonDataKeys.EDITOR)
+        val selectionModel = editor.selectionModel
+        val selectedText = selectionModel.selectedText
+
+        // Kiểm tra nếu có văn bản được chọn
+        if (!selectedText.isNullOrEmpty()) {
+            // Xoa dau
+            val convertedText = UnicodeUtils.removeAccent(selectedText)
+
+            // Lấy Document và thay thế văn bản đã chọn
+            e.project?.let { replaceTextInEditor(it, editor, selectionModel, convertedText) }
+        } else {
+            Messages.showMessageDialog(
+                e.project,
+                "No text selected!",
+                "Error",
+                Messages.getErrorIcon()
+            )
+        }
+    }
+
+    fun replaceTextInEditor(project: Project, editor: Editor, textRange: SelectionModel, newText: String) {
+        // Sử dụng WriteCommandAction để đảm bảo quyền truy cập ghi trong IntelliJ
+        WriteCommandAction.runWriteCommandAction(project) {
+            editor.document.replaceString(textRange.selectionStart, textRange.selectionEnd, newText)
+        }
+    }
+}
